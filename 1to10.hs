08991e97a6dc244d6eded346a36af8f6b8b4187a
Adrian Nilsson <adnilsson@users.noreply.github.com>
2015-07-02 22:13:36 +0200

    Done with P3-P6

--- 1to10.hs
+++ 1to10.hs
@@ -2,9 +2,7 @@
 -- 1. (*) Find the last element of a list
 -----
 myLast::(Show a) => [a] -> a
-myLast []        = error "An empty list has no end"
-myLast (x:[])    = x
-myLast (x:xs)    = myLast xs
+myLast  = foldl1 (\_ x -> x)
 
 -- 2. (*) Find the second to last element
 -----
@@ -16,6 +14,29 @@ myButLast (x:xs)    = myButLast xs
 
 -- 3. (*) Find the K'th element of a list, indexed from 1
 -----
---elementAt:: [a] b -> a
---elementAt 
+elementAt:: [a] -> Int -> a
+elementAt xs n  = head .  drop (n-1) $ xs
 
+-- 4. (*) Find the number of elements in a list
+-----
+myLength:: [a] -> Int
+myLength = foldl (\acc _ -> acc+1) 0
+
+-- 5. (*) Reverse a list
+-----
+myReverse:: [a] -> [a]
+myReverse = foldl(\acc x -> x:acc) []
+
+-- 6. (*) Determine if a list is a palindrome
+-----
+isPalindrome:: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == myReverse xs
+
+-- 7. (*) Flatten a nested list structure
+-----
+data NestedList a = Elem a | List [NestedList a]
+
+flatten:: NestedList a -> [a] 
+flatten (Elem x)        = [x]
+flatten (List [])       = []
+flatten (List (x:xs))   = 
